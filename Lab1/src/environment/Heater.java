909ae532f13291793761701152ac78e690e25cf2
uansett <stian.uansett@gmail.com>
2012-10-09 16:17:38 +0100

    small steps right dir

--- Lab1/src/environment/Heater.java
+++ Lab1/src/environment/Heater.java
@@ -1,5 +1,12 @@
 package environment;
 
 public class Heater {
+	
+private double thermostatValue;
+private double heatingFactor;
 
+	public Heater(double thermostatValue, double heatingFactor){
+		this.thermostatValue = thermostatValue;
+		this.heatingFactor = heatingFactor;
+	}
 }
--- Lab1/src/environment/Room.java
+++ Lab1/src/environment/Room.java
@@ -2,4 +2,20 @@ package environment;
 
 public class Room extends Environment{
 private Heater heater;
+private double areaInSqMtr;
+private double leakConstant;
+
+public Room(Heater heater, double roomAreaInSqMtr, double leakConstant){
+	this.heater = heater;
+	this.areaInSqMtr = roomAreaInSqMtr;
+}
+
+public void leakTemperature(double outsideTemp){
+	this.temperature -= leakAmount(this.temperature, outsideTemp,this.leakConstant);
+}
+
+private double leakAmount(double insideTemp, double outsideTemp, double leakConstant){
+	return ((insideTemp - outsideTemp)*leakConstant);
+}
+
 }
